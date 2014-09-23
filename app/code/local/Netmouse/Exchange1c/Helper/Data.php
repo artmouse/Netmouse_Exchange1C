8c6823d55ab7c1a866e33954446d55b46e91e05a
artmouse <artmouse@ukr.net>
2014-09-23 23:20:14 +0300

    First Commit

--- app/code/local/Netmouse/Exchange1c/Helper/Data.php
+++ app/code/local/Netmouse/Exchange1c/Helper/Data.php
@@ -1,10 +1,24 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: netmouse
- * Date: 23.09.14
- * Time: 16:44
- */ 
+ * Netmouse
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Open Software License (OSL 3.0)
+ * that is bundled with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/osl-3.0.php
+ * If you did not receive a copy of the license and are unable to
+ * obtain it through the world-wide-web, please send an email
+ * to license@netmouse.com.ua so we can send you a copy immediately.
+ *
+ *
+ * @category    Netmouse
+ * @package     Exchange1c
+ * @copyright   Copyright (c) 2014 Netmouse http://netmouse.com.ua
+ * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
+ */
+
 class Netmouse_Exchange1c_Helper_Data extends Mage_Core_Helper_Abstract {
 
 }
--- app/code/local/Netmouse/Exchange1c/controllers/IndexController.php
+++ app/code/local/Netmouse/Exchange1c/controllers/IndexController.php
@@ -15,16 +15,8 @@
  *
  * @category    Netmouse
  * @package     Exchange1c
- * @copyright   Copyright (c) 2013 Netmouse http://netmouse.com.ua
+ * @copyright   Copyright (c) 2014 Netmouse http://netmouse.com.ua
  * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
- */
-
-/**
- * 1c front controller
- *
- * @category Netmouse
- * @package Exchange1c
- * @author Netmouse http://netmouse.com.ua
  */
 
 class Netmouse_Exchange1c_IndexController extends Mage_Core_Controller_Front_Action
@@ -32,8 +24,136 @@ class Netmouse_Exchange1c_IndexController extends Mage_Core_Controller_Front_Act
 {
     public function indexAction()
     {
+        $type = $this->getRequest()->getParam('type');
+        $mode = $this->getRequest()->getParam('mode');
 
-        echo "Hello !!!";
+        switch ($type) {
+            case "catalog":
+                switch ($mode) {
+                    case "checkauth":
+                        $this->catalogCheckauthAction();
+                        break;
+                    case "init":
+                        $this->catalogInitAction();
+                        break;
+                    case "file":
+                        $this->catalogFileAction();
+                        break;
+                    case "import":
+                        $this->catalogImportAction();
+                        break;
+                    default:
+                        echo "failure" . PHP_EOL . "Invalid exchange catalog mode parameter";
+                        break;
+                }
+
+                break;
+            case "sale":
+                switch ($mode) {
+                    case "checkauth":
+                        $this->saleCheckauthAction();
+                        break;
+                    case "init":
+                        $this->saleInitAction();
+                        break;
+                    case "query":
+                        $this->saleQueryAction();
+                        break;
+                    case "success":
+                        $this->saleSuccessAction();
+                        break;
+                    case "file":
+                        $this->saleFileAction();
+                        break;
+                    default:
+                        echo "failure" . PHP_EOL . "Invalid exchange sale mode parameter";
+                        break;
+                }
+
+                break;
+            default:
+                echo "failure" . PHP_EOL . "Invalid exchange type parameter";
+                break;
+        }
+    }
+
+    protected function _getZipEnabled()
+    {
+        return "no";
+    }
+
+    protected function _getFileLimit()
+    {
+        return 1024000;
+    }
+
+    protected function _isAuthorized()
+    {
+        $name = Mage::getSingleton('core/session')->getSessionName();
+
+        $value = $this->getRequest()->getCookie($name);
+        if (null === $value) {
+            $value = $this->getRequest()->getParam($name);
+        }
+
+        if (Mage::getSingleton('core/session')->getSessionId() == $value) {
+            return true;
+        }
+
+        echo "failure" . PHP_EOL;
+        echo "Session has expired";
+        return false;
+    }
+
+    public function catalogCheckauthAction()
+    {
+        echo "success" . PHP_EOL;
+        echo Mage::getSingleton('core/session')->getSessionName() . PHP_EOL;
+        echo Mage::getSingleton('core/session')->getSessionId();
+    }
+
+    public function catalogInitAction()
+    {
 
     }
+
+    public function catalogFileAction()
+    {
+
+    }
+
+    public function catalogImportAction()
+    {
+
+    }
+
+
+    public function saleCheckauthAction()
+    {
+        echo "success" . PHP_EOL;
+        echo Mage::getSingleton('core/session')->getSessionName() . PHP_EOL;
+        echo Mage::getSingleton('core/session')->getSessionId();
+    }
+
+    public function saleInitAction()
+    {
+
+    }
+
+
+    public function saleQueryAction()
+    {
+
+    }
+
+    public function saleSuccessAction()
+    {
+
+    }
+
+    public function saleFileAction()
+    {
+
+    }
+
 }
